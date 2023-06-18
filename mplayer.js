85b5b784d8ad25a7bbb621b555bb304bca3f96f5
yorry2101 <98647947+yorry2101@users.noreply.github.com>
2023-06-19 01:07:29 +0900

    画像切り替え機能追加

--- mplayer.js
+++ mplayer.js
@@ -17,3 +17,18 @@ for (var i=0; i<listitems.length; i++){
         }
     );
 }
+
+// 再生中と停止中でイラストを切り替える
+var audio = document.querySelector('audio');
+audio.addEventListener('play',
+    (e)=>{
+        var img = document.querySelector('img');
+        img.setAttribute('src', 'arona_portrait.png');
+    }
+);
+audio.addEventListener('pause',
+    (e)=>{
+        var img = document.querySelector('img');
+        img.setAttribute('src', 'arona_portrait_2.png');
+    }
+);
